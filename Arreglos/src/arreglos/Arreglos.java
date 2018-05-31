5394b7d6f12733b485263561056388eaf845687f
Jason Castillo <jasson_72@hotmail.com>
2018-05-31 10:15:12 -0500

    Agregando trabajo en clases

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] c = new int[10];
+        c[0] = 10;
+
+        for (int contador = 0; contador < c.length; contador++) {
+            System.out.println(c[contador]);
+        }
+
+    }
+}
