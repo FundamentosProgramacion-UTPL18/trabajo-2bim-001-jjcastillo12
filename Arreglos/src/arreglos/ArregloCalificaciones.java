0d71ee9aa3b62c95b69932cfaf3cbc47331f093a
Jason Castillo <jasson_72@hotmail.com>
2018-05-31 11:52:39 -0500

    Agregando Ejercicios

--- Arreglos/src/arreglos/ArregloCalificaciones.java
+++ Arreglos/src/arreglos/ArregloCalificaciones.java
@@ -5,8 +5,6 @@
  */
 package arreglos;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Usuario
@@ -14,16 +12,17 @@ import java.util.Scanner;
 public class ArregloCalificaciones {
 
     public static void main(String[] args) {
-        String estudiante;
-        String nombre;
-        double[] calificaciones_progra = {18, 19, 15, 16, 17, 10};
-        double[] calificaciones_bd = {10, 12, 13, 20, 17, 20};
-        double[] promedios = new double[6];
+        // Listas 
+        double[] calificaciones_progra = {18, 19, 15, 16, 17, 10};// calificaciones de programacion
+        double[] calificaciones_bd = {10, 12, 13, 20, 17, 20}; // calificaciones de base de datos
+        double[] promedios = new double[6]; // promedios
+        // Operaciones de promedio
         for (int contador = 0; contador < calificaciones_bd.length; contador++) {
             double suma = calificaciones_progra[contador] + calificaciones_bd[contador];
             double promedio = suma / 2;
             promedios[contador] = promedio;
         }
+        //Operaciones de impresion
         for (int contador2 = 0; contador2 < promedios.length; contador2++) {
             System.out.printf("Estudiante %d:\t%.2f\t%.2f\t%.2f\n", contador2+1, calificaciones_progra[contador2], calificaciones_bd[contador2], promedios[contador2]);
         }
